10e4c3b76dd17ada23354651b3b7941e8908a711
Loris <devloris@gmail.com>
2020-04-19 19:53:45 +0200

    :bug: Fix audio

--- src/js/models/audio/audio-dictionnary.js
+++ src/js/models/audio/audio-dictionnary.js
@@ -3,8 +3,6 @@
  * A sound has a required file. loop and volume are optional
  */
 const AUDIO_DICTIONARY = {
-    'vocal_1' : {file: "sounds/vocal/vocal1.m4a"},
-    'vocal_2' : {file: "sounds/vocal/vocal2.m4a"},
     'birds' : {file: "sounds/birds.mp3", loop: false, volume: 0.1},
     'horn' : {file: "sounds/horn.wav", volume: 0.2},
     'paper' : {file: "sounds/paper.wav", volume: 0.5},
