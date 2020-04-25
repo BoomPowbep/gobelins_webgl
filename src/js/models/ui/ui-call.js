d84679226bd631cf666148d22f03da235e067ddd
Loris <devloris@gmail.com>
2020-04-25 11:51:58 +0200

    :sparkles: Add UI for Phone

---
+++ src/js/models/ui/ui-call.js
@@ -0,0 +1,22 @@
+import Ui from "./ui";
+
+class UiCall extends Ui {
+    constructor() {
+        super("#app_call");
+    }
+
+    setupGlobalEvents() {
+        super.setupGlobalEvents();
+
+    }
+
+    setupDOM() {
+        super.setupDOM();
+    }
+
+    setupEvents() {
+        super.setupEvents();
+    }
+}
+
+export default UiCall;
