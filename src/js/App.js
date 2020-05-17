a8a857b9622e2a2adc04589a6ce89d43b82a6073
Loris <devloris@gmail.com>
2020-05-17 18:42:50 +0200

    :sparkles: Add Police People

--- src/js/App.js
+++ src/js/App.js
@@ -4,7 +4,7 @@ class App {
     constructor() {
         console.log('🏗 App constructor');
 
-        this.game = new Game(false);
+        this.game = new Game(true);
     }
 
     init() {
--- src/js/Game/Game.js
+++ src/js/Game/Game.js
@@ -679,7 +679,7 @@ export default class Game {
                 identifier: 'BarBarman',
                 path: 'models/FBX/Bar_Barman.fbx',
                 initialScaleFactor: 0.15,
-                initialPosition: {x: -96, y: 16, z: 31},
+                initialPosition: {x: -92, y: 9, z: 31},
                 initialRotation: {x: 0, y: toRad(180), z: 0}
             }),
             new Model({
@@ -770,7 +770,7 @@ export default class Game {
         function setupDatGUIModels() {
             let editedElement = GameBrain.modelManager.getModelReferenceByIdentifier('Colleuse1');
             let identifier = {model: ""};
-            let elementSelector = GameBrain.gui.add(identifier, 'model', ['Colleuse2', 'Colleuse3', 'Colleuse1', 'PoliceConversationGauge', 'ColleuseConversationGauge', 'StreetPassant', 'BarClient', 'BarBarman', 'letter-1', 'letter-2', 'letter-3', 'letter-4', 'letter-5', 'letter-6', 'letter-7', 'letter-8', "map-interest-1", "map-interest-2", "map-interest-3", "map-interest-final", "BistroConversationGauge"]);
+            let elementSelector = GameBrain.gui.add(identifier, 'model', ['Police_Texting', 'Police_Smoking', 'Colleuse2', 'Colleuse3', 'Colleuse1', 'PoliceConversationGauge', 'ColleuseConversationGauge', 'StreetPassant', 'BarClient', 'BarBarman', 'letter-1', 'letter-2', 'letter-3', 'letter-4', 'letter-5', 'letter-6', 'letter-7', 'letter-8', "map-interest-1", "map-interest-2", "map-interest-3", "map-interest-final", "BistroConversationGauge"]);
 
             let x_element = null;
             let y_element = null;
@@ -872,6 +872,24 @@ export default class Game {
                 initialPosition: {x: 0, y: -100000, z: 0},
                 initialRotation: {x: 0, y: toRad(-90), z: 0}
             }),
+            new Model({
+                identifier: 'Police_Texting',
+                path: 'models/FBX/Police_Texting.fbx',
+                initialScaleFactor: 1,
+                initialRotation: {x: 0, y: 3.4, z: 0},
+                initialPosition: {
+                    x: 61, y: -20, z: -125
+                },
+            }),
+            new Model({
+                identifier: 'Police_Smoking',
+                path: 'models/FBX/Police_Smoking.fbx',
+                initialScaleFactor: 1,
+                initialPosition: {
+                    x: 61, y: -20, z: -146
+                },
+            }),
+
         ];
 
         lights = [];
@@ -888,6 +906,9 @@ export default class Game {
                     onLoadDone: (scenery) => {
                         ready++;
                         checkElementsReady();
+
+                        GameBrain.modelManager.playFirstAnimationByIdentifier("Police_Smoking");
+                        GameBrain.modelManager.playFirstAnimationByIdentifier("Police_Texting");
 
                         let sceneModel = GameBrain.modelManager.getModelReferenceByIdentifier("ComissariatEnvironment");
                         let kangoo = GameBrain.modelManager.getModelReferenceByIdentifier("Kangoo");
@@ -925,7 +946,7 @@ export default class Game {
             )
         );
 
-        function checkElementsReady() {
+        let checkElementsReady = () => {
             const duration = 1;
             let total = GameBrain.sceneryManager._sceneries.length;
 
