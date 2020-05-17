078adc73fbb0f991ab75532338c3df7a52bd5186
Debalme Mickaël <mickael.debalme@gmail.com>
2020-05-17 22:04:23 +0200

    Added instagram posts

--- src/js/models/variables/instagram-post-var.js
+++ src/js/models/variables/instagram-post-var.js
@@ -3,27 +3,25 @@ const INSTAGRAM_POSTS_VARS = {
         name: 'collage.feminicide',
         commentary : 'Scène bar',
         images : [
-            'images/slides/slide_1.png',
-            'images/slides/slide_2.png',
-            'images/slides/slide_3.png',
+            'images/slides/bar_1.png',
+            'images/slides/bar_2.png',
         ]
     },
     'post-2': {
         name: 'collage.feminicide',
         commentary : 'Scène collage',
         images : [
-            'images/slides/slide_1.png',
-            'images/slides/slide_2.png',
+            'images/slides/collage_1.png',
+            'images/slides/collage_2.png',
         ]
     },
     'post-3': {
         name: 'collage.feminicide',
         commentary : 'Scène commico',
         images : [
-            'images/slides/slide_1.png',
-            'images/slides/slide_3.png',
-            'images/slides/slide_2.png',
-            'images/slides/slide_3.png',
+            'images/slides/commico_1.png',
+            'images/slides/commico_2.png',
+            'images/slides/commico_3.png',
         ]
     },
 }
